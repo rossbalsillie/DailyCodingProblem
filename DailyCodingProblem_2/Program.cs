c3e9bd7f67b68fd1ec8c218ce623f9b5530ce611
Ross Balsillie <ross.balsillie@c2software.com>
2019-04-12 11:04:23 +0100

    Comments added in. Edge case question added for 0 integer value being inputted by a user.

--- DailyCodingProblem_2/Program.cs
+++ DailyCodingProblem_2/Program.cs
@@ -7,6 +7,7 @@ Given an array of integers, return a new array such that each element at index i
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
 Follow-up: what if you can't use division?
+Follow-up: what if one of the integers inputted was a 0, how should this be handled?
 */
 
 using System;
@@ -21,8 +22,10 @@ namespace DailyCodingProblem_2
     {
         static void Main(string[] args)
         {
+            //Get the list of integers from the user
             List<int> inputtedIntegers = GetListOfNumbersInput();
-            List<int> productList = OutputNewListOfArray(inputtedIntegers);
+            //Create a new list of integers which is the product of all values except the one at index[i]
+            List<int> productList = OutputNewListOfIntegers(inputtedIntegers);
             foreach (int number in productList)
             {
                 Console.WriteLine(number);
@@ -30,32 +33,35 @@ namespace DailyCodingProblem_2
             Console.ReadKey();
         }
 
-        private static List<int> OutputNewListOfArray(List<int> initialList)
+        private static List<int> OutputNewListOfIntegers(List<int> initialList)
         {
             List<int> productList = new List<int>();
 
+            //Outer loop
             for (int i = 0; i < initialList.Count; i++)
             {
+                //Set the product number to 0 & the initial number
                 int productNumber = 0;
                 int initialNumber = 0;
+                //Inner loop
                 for (int j = 0; j < initialList.Count; j++)
-                {                    
-                    if (j == i)
+                {
+                    if (j == i) //If the inner loop and outter loop are at the same point then skip over this index
                     {
                         continue;
                     }
-                    else if (initialNumber > 0 && productNumber == 0)
+                    else if (initialNumber > 0 && productNumber == 0) //If we have an initial number but the product number is 0 then use the initial number for the first multiplication
                     {
                         productNumber = initialNumber * initialList[j];
                     }
-                    else if (initialNumber > 0 && productNumber > 0)
+                    else if (initialNumber > 0 && productNumber > 0) //We have an initial number and the product number is greater than 0 so we use the product number in the multiplication
                     {
                         productNumber = productNumber * initialList[j];
                     }
-                    else
+                    else // If we hit this condition then it means the initial number has not yet been set
                     {
                         initialNumber = initialList[j];
-                    }                    
+                    }
                 }
                 productList.Add(productNumber);
             }
@@ -63,6 +69,7 @@ namespace DailyCodingProblem_2
             return productList;
         }
 
+        //Gets the list of integers inputted from the user and casts them into a new list of ints
         private static List<int> GetListOfNumbersInput()
         {
             Console.WriteLine("Please Provide a list of space separated numbers!");
