051ca5ab3d2ac00462f5d4ef581931ed4c6413a9
Ross Balsillie <ross.balsillie@c2software.com>
2019-05-03 09:05:08 +0100

    New unit test project added to work on DCP 4

---
+++ DailyCodingProblemTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DailyCodingProblemTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Find_Lowest_Positive_Integer()
+        {
+
+        }
+    }
+}
