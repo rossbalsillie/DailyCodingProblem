b67eebbf0bb113907171e693d279ce09a11932f0
Ross Balsillie <ross.balsillie@c2software.com>
2019-05-22 09:14:34 +0100

    Daily Coding Problem 5 setup

---
+++ DailyCodingProblem_5/cons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyCodingProblem_5
+{
+    class Cons
+    {
+        public Array Construct(string a, string b)
+        {
+
+        }
+    }
+}
