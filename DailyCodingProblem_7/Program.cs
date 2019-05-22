bb304de92b73b17a2a4893a54f72d27648e16b18
Ross Balsillie <ross.balsillie@c2software.com>
2019-05-22 11:52:11 +0100

    DCP7 initial setup

---
+++ DailyCodingProblem_7/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Facebook.
+
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.*/
+using System;
+
+namespace DailyCodingProblem_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
